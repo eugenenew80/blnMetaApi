cad051b52751d8af21f233151676f6d595798385
eugenenew80 <eugenenew80@gmail.com>
2017-09-30 11:24:45 +0600

    -

--- src/main/java/kz/kegoc/bln/entity/meta/dto/DictDto.java
+++ src/main/java/kz/kegoc/bln/entity/meta/dto/DictDto.java
@@ -1,8 +1,12 @@
 package kz.kegoc.bln.entity.meta.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class DictDto {
 	private Long id;
 	private String code;
